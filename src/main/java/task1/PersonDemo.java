ba68502712e9d7d1b7c0729c21bf6cbf1b52cca3
Tomasz Kasprzycki <kasprzyckit@gmail.com>
2017-11-27 22:35:03 +0100

    The Person objects factory implemented.

--- src/main/java/task1/PersonDemo.java
+++ src/main/java/task1/PersonDemo.java
@@ -1,5 +1,7 @@
 package task1;
 
+import java.util.ListIterator;
+
 import static java.lang.System.*;
 
 public class PersonDemo {
@@ -8,6 +10,9 @@ public class PersonDemo {
         try {
             Person person = new Person("John", "Smith", "smith.john@gmail.com");
             out.println(person);
+            PersonFactory factory = new PersonFactory();
+            ListIterator<Person> it = factory.createRandomPersonList(30).listIterator();
+            while (it.hasNext()) out.println(it.next());
         }
         catch (Exception e) {
             e.printStackTrace();
--- src/main/java/task1/PersonFactory.java
+++ src/main/java/task1/PersonFactory.java
@@ -1,8 +1,66 @@
 package task1;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class PersonFactory {
+    private List<String> firstNames = new LinkedList<>();
+    private List<String> lastNames = new LinkedList<>();
+    private String[] emails = {"@gmail.com", "@yahoo.com", "@company.fr", "@usembassy.eu"};
+
+    public PersonFactory()
+    {
+        BufferedReader br = null;
+        String name;
+
+        try{
+            br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("firstnames.txt")));
+            while ((name = br.readLine()) != null) firstNames.add(name);
+
+            br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("lastnames.txt")));
+            while ((name = br.readLine()) != null) lastNames.add(name);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (br != null)
+                    br.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
     public Person createRandomPerson()
     {
+        Random random = new Random();
+        String firstName = firstNames.get(random.nextInt(firstNames.size()));
+        String lasttName = lastNames.get(random.nextInt(lastNames.size()));
+        return new Person(firstName, lasttName, generateEmail(firstName, lasttName));
+    }
+
+    public LinkedList<Person> createRandomPersonList(int n)
+    {
+        LinkedList<Person> persons = new LinkedList<>();
+        for (int i = 0; i<n; i++) persons.add(createRandomPerson());
+        return persons;
+    }
+
+    private String generateEmail(String firstName, String lastName)
+    {
+
+        int emailFormat = firstName.hashCode()*29 + lastName.hashCode()*13;
+        switch (Math.abs(emailFormat)%4){
+            case 0: return firstName.toLowerCase() + lastName.toLowerCase() + emails[(firstName.length()*lastName.length())%4];
+            case 1: return lastName.toLowerCase() + "." + firstName.toLowerCase() + emails[(firstName.length()*lastName.length())%4];
+            case 2: return firstName.substring(0,1).toLowerCase() + lastName.toLowerCase() + emails[(firstName.length()*lastName.length())%4];
+            case 3: return lastName.toLowerCase() + Integer.toString((emailFormat)%100+100) + emails[(firstName.length()*lastName.length())%4];
+        }
         return null;
     }
 }
